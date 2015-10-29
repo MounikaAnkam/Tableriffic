0cb0f5e7216350bdd2db600e95c4efb07eb411a8
MounikaAnkam <mounikaankam0330@gmail.com>
2015-10-29 09:44:29 -0500

    Working code of Tableriffic Assignment

---
+++ Tableriffic/AddNewEquipmentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddNewEquipmentViewController.swift
+//  Tableriffic
+//
+//  Created by Michael Rogers on 2/22/15.
+//  Copyright (c) 2015 Michael Rogers. All rights reserved.
+//
+
+import UIKit
+
+class AddNewEquipmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tableriffic/EquipmentVC.swift
@@ -0,0 +1,35 @@
+//
+//  EquipmentVC.swift
+//  Tableriffic
+//
+//  Created by Michael Rogers on 2/19/15.
+//  Copyright (c) 2015 Michael Rogers. All rights reserved.
+//
+
+import UIKit
+
+class EquipmentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
